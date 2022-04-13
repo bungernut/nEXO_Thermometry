1deb9829311ed9a4eec7d5ac660736a13c73bdae
bungernut <bungernut@gmail.com>
2022-04-12 17:33:20 -0700

    added bme values to tft and modbus

--- teensy_tft/teensy_tft.ino
+++ teensy_tft/teensy_tft.ino
@@ -19,11 +19,11 @@ Adafruit_MAX31865 rtd02 = Adafruit_MAX31865(41);
 Adafruit_MAX31865 rtd03 = Adafruit_MAX31865(18);
 Adafruit_MAX31865 rtd04 = Adafruit_MAX31865(34);
 Adafruit_MAX31865 rtd05 = Adafruit_MAX31865(33);
-// Adafruit_MAX31856 tcp01 = Adafruit_MAX31856(24);
-// Adafruit_MAX31856 tcp02 = Adafruit_MAX31856(29);
-// Adafruit_BME280 bme(27); // hardware SPI
+//Adafruit_MAX31856 tcp01 = Adafruit_MAX31856(24);
+//Adafruit_MAX31856 tcp02 = Adafruit_MAX31856(29);
+Adafruit_BME280 bme(27); // hardware SPI
 
-#define VERSION "nEXO Thermometry v1.3"
+#define VERSION "nEXO Thermometry v1.4"
 #define TFT_DC 36
 #define TFT_CS 40
 #define T_CS 37
@@ -33,7 +33,7 @@ ILI9341_t3 tft = ILI9341_t3(TFT_CS, TFT_DC);
 // Enter a MAC address for your controller below.
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 // The IP address will be dependent on your local network:
-IPAddress ip(192, 168, 1, 81);
+IPAddress ip(10, 0, 0, 81);
 
 EthernetServer ethServer(502);
 EthernetClient client;
@@ -60,20 +60,30 @@ float rtd05temp;
 float tcp01temp;
 float tcp02temp;
 float bmeTemp;
+float bmePres;
+float bmeHumd;
 
 //Modbus holding register address mapping
-const int rtd01_reg = 0x00;
-const int rtd02_reg = 0x01;
-const int rtd03_reg = 0x02;
-const int rtd04_reg = 0x03;
-const int rtd05_reg = 0x04;
-const int tcp01_reg1 = 0x05;
-const int tcp01_reg2 = 0x06;
-const int tcp02_reg1 = 0x07;
-const int tcp02_reg2 = 0x08;
-const int bme_reg1 = 0x09;
-const int bme_reg2 = 0x010;
+const int rtd01_reg = 0;
+const int rtd02_reg = 1;
+const int rtd03_reg = 2;
+const int rtd04_reg = 3;
+const int rtd05_reg = 4;
+const int tcp01_reg1 = 5;
+const int tcp01_reg2 = 6;
+const int tcp02_reg1 = 7;
+const int tcp02_reg2 = 8;
+const int bmeTmp_reg1 = 9;
+const int bmeTmp_reg2 = 10;
+const int bmePrs_reg1 = 11;
+const int bmePrs_reg2 = 12;
+const int bmeHum_reg1 = 13;
+const int bmeHum_reg2 = 14;
 
+union {
+    float asFloat;
+    uint16_t asInt[2];
+} flreg;
 
 void setup() {
   Serial.begin(9600);
@@ -102,7 +112,7 @@ void setup() {
   //tcp01.setThermocoupleType(MAX31856_TCTYPE_T);
   //tcp02.begin();
   //tcp02.setThermocoupleType(MAX31856_TCTYPE_T);
-  //bme.begin();
+  bme.begin();
   
   // Get the current values for the sensors before serving crap data
   readSensors();
@@ -190,7 +200,10 @@ void readSensors() {
   rtd05temp = rtd05.temperature(RNOMINAL, RREF);
   //tcp01temp = tcp01.readThermocoupleTemperature();
   //tcp02temp = tcp02.readThermocoupleTemperature();
-  //bmeTemp = bme.readTemperature();
+  bmeTemp = bme.readTemperature();
+  bmePres = float(bme.readPressure())/100000.0;
+  bmeHumd = bme.readHumidity();
+  
 
   // Change Celsius to Kelvin
   rtd01temp += 273.15;
@@ -200,7 +213,7 @@ void readSensors() {
   rtd05temp += 273.15;
   //tcp01temp += 273.15;
   //tcp02temp += 273.15;
-  //bmeTemp += 273.15;
+  bmeTemp += 273.15;
 }
 
 
@@ -251,9 +264,15 @@ void updateClient() {
   //flreg.asFloat = tcp02temp;
   //modbusTCPServer.holdingRegisterWrite(tcp02_reg1, flreg.asInt[1]);
   //modbusTCPServer.holdingRegisterWrite(tcp02_reg2, flreg.asInt[0]);
-  //flreg.asFloat = bmeTemp;
-  //modbusTCPServer.holdingRegisterWrite(bme_reg1, flreg.asInt[1]);
-  //modbusTCPServer.holdingRegisterWrite(bme_reg2, flreg.asInt[0]);
+  flreg.asFloat = bmeTemp;
+  modbusTCPServer.holdingRegisterWrite(bmeTmp_reg1, flreg.asInt[1]);
+  modbusTCPServer.holdingRegisterWrite(bmeTmp_reg2, flreg.asInt[0]);
+  flreg.asFloat = bmePres;
+  modbusTCPServer.holdingRegisterWrite(bmePrs_reg1, flreg.asInt[1]);
+  modbusTCPServer.holdingRegisterWrite(bmePrs_reg2, flreg.asInt[0]);
+  flreg.asFloat = bmeHumd;
+  modbusTCPServer.holdingRegisterWrite(bmeHum_reg1, flreg.asInt[1]);
+  modbusTCPServer.holdingRegisterWrite(bmeHum_reg2, flreg.asInt[0]);  
 }
 
 
@@ -314,12 +333,7 @@ void updateDisplay() {
 
   tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
   tft.setCursor(10, 220);
-  tft.setTextSize(2);
-  tft.print("BME_280:"); tft.print(bmeTemp); tft.println(" K");
+  tft.setTextSize(1);
+  tft.print("BME:"); tft.print(bmeTemp); tft.print("K "); 
+  tft.print(bmePres); tft.print("Bar "); tft.print(bmeHumd); tft.println("%");
 }
-
-
-union {
-    float asFloat;
-    uint16_t asInt[2];
-} flreg;
